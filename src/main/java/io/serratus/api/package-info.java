f0d2a66b6ae4cf4d0bc99d209ab4e657f5b4095f
manaswinidas <dasmanaswini10@gmail.com>
2021-10-26 19:16:38 +0530

    Create package-info.java class for vertx api code generation

---
+++ src/main/java/io/serratus/api/package-info.java
@@ -0,0 +1,6 @@
+// Register the io.serratus.api package for the Vert.x API code generation plugin to find the API classes to generate event bus code for.
+@ModuleGen(name="serratus-api", groupPackage="io.serratus.api")
+package io.serratus.api;
+
+// Import the Vert.x code generation module package.
+import io.vertx.codegen.annotations.ModuleGen;
